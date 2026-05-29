cea64c742c2c2878deb07ff95643d85d42dcb4f4
kgbwilde <kgbdracowilde@gmail.com>
2026-05-29 12:30:04 +0000

    v0.3 (formatação da lista)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-lista de itens = [nome, tipo de exame, data do exame, valor do exame, resultado do exame]
+lista_de_itens = []
 
 while True:
     print("Bem-vindo ao sistema de gerenciamento de exames laboratoriais!")
+    print("==============================================================")
     print("1. Cadastro de exames")
     print("2. Consulta de exames")
     print("3. Sair")
@@ -14,8 +15,8 @@ while True:
             nome = input("Digite o nome do paciente: ")
             tipo_de_exame = input("Digite o tipo de exame: ")
             valor_do_exame = input("Digite o valor do exame: ")
-            data_do_exame = input("Digite a data: dia/mes/ano")
-            hora do exame = input("Digite a hora do exame: 00:00 am/pm")
+            data_do_exame = int(input("Digite a data: dia/mes/ano"))
+            hora_do_exame = int(input("Digite a hora do exame: 00:00 am/pm"))
             resultado_do_exame = input("Digite o resultado do exame: ")
 
             novo_item = {
@@ -30,11 +31,13 @@ while True:
             print("Item cadastrado com sucesso")
 
         case 2:
+            print("-" * 20)
             for item in lista_itens: # o que será mostrado na tela para o usuário
-
+                print("=" * 20)
                 print(f"Nome: {item["nome"]}")
                 print(f"Exame: {item["tipo_de_exame"]}")
                 print(f"Valor: {item["valor_do_exame"]}")
                 print(f"Data: {item["data_do_exame"]}")
                 print(f"Hora; {item["hora_do_exame"]}")
                 print(f"Resultado: {item["resultado_do_exame"]}")
+                print("=" * 20) 
